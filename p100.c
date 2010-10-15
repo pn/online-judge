c374e3fd378ae325b49833999ec82bc786de36e2
Paweł Nadolski <Pawel.Nadolski@alcatel-lucent.com>
2010-10-15 15:37:10 +0200

    accepted solution for problem 100

--- p100.c
+++ p100.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
-#define CACHE_SIZE 1000000
-int cyc_len[CACHE_SIZE];
+#include <string.h>
+#define CACHE_SIZE 500000
+#define TYP unsigned int
+#define GETVAL(n) (n%2?3*n+1:n/2)
+TYP cyc_len[CACHE_SIZE];
 
-int getValue(int n) {
+TYP getValue(TYP n) {
 	if(n < CACHE_SIZE) {
-		if(cyc_len[n] > 0)
+		if(cyc_len[n] > 0) {
 			return cyc_len[n];
-		else
-			return cyc_len[n] = 1 + getValue(n%2?3*n+1:n/2);
-	} else return 1 + getValue(n%2?3*n+1:n/2);
+		} else {
+			return cyc_len[n] = 1 + getValue(GETVAL(n));
+		}
+	} else {
+		return 1 + getValue(GETVAL(n));
+	}
 }
 
+TYP i, j, k, x;
 int main() {
-	int i, j, k, x;
-	//memset(cyc_len, 0, sizeof(int)*CACHE_SIZE);
 	cyc_len[1] = 1;
 
-	while(1) {
-		x = fscanf(stdin, "%d %d\n", &i, &j);
-		if(x == EOF) break;
-		int max = 0;
-		for(k = i; k <= j; k++) {
-			int val = getValue(k);
-			if(val > max) max = val;
+	while((x = fscanf(stdin, "%d %d\n", &i, &j)) != EOF) {
+		TYP max = 0, maxnum = 0;
+		for(k = (i<j?i:j); k <= (j>i?j:i); k++) {
+			TYP val = getValue(k);
+			if(val > max) { max = val; maxnum = k;}
 		}
 		printf("%d %d %d\n", i, j, max);
 	}
